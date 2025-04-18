62b2420f8f2f5fb6e02cbf2d02f09a009df1c275
Navitha Sri Kamisetty <153588808+NAVITHASRI-KAMISETTY@users.noreply.github.com>
2025-04-18 18:22:36 +0530

    Create Problem-3.py

---
+++ Problem-3.py
@@ -0,0 +1,15 @@
+a = int(input())
+i = 1
+count = 0
+
+if a % 2 == 0:
+    limit = a - 1
+else:
+    limit = a
+
+while count < limit:
+    print(i, end="")
+    count += 1
+    if count < limit:
+        print(",", end="")
+    i += 2
