82092080a0a17e2f8875ff1fccd0545b6b0ecdfb
Navitha Sri Kamisetty <153588808+NAVITHASRI-KAMISETTY@users.noreply.github.com>
2025-04-20 09:04:03 +0530

    Update Problem-4.py

--- Problem-4.py
+++ Problem-4.py
@@ -1,12 +1,11 @@
-input_list = input().strip()
-if input_list.startswith('[') and input_list.endswith(']'):
-    input_list = input_list[1:-1]
-input_list = [int(num.strip()) for num in input_list.split(",")]
-count_dict = {}
+nums = list(map(int, input().strip("[]").split(",")))
+result = {}
+
 for i in range(1, 10):
     count = 0
-    for num in input_list:
-        if num % i == 0:  
-            count += 1  
-    count_dict[i] = count  
-print(count_dict)
+    for num in nums:
+        if num % i == 0:
+            count += 1
+    result[i] = count
+
+print(result)
